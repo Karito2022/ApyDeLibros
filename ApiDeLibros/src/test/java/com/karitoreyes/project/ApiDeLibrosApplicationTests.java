864870e4445407dbe39a0a5a112f51978f441e4b
Karito2022 <reyesbarrerakaren@gmail.com>
2022-03-12 18:21:30 -0500

    Practica terminada

---
+++ ApiDeLibros/src/test/java/com/karitoreyes/project/ApiDeLibrosApplicationTests.java
@@ -0,0 +1,14 @@
+package com.karitoreyes.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
